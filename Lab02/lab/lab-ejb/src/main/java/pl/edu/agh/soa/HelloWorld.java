1bfdb0e26958e60173d8a3f8f28bb5d356665c8e
Rejurhf <mziomek108@gmail.com>
2019-03-20 07:30:30 +0100

    Authentication almost doing good

--- Lab02/lab/lab-ejb/src/main/java/pl/edu/agh/soa/HelloWorld.java
+++ Lab02/lab/lab-ejb/src/main/java/pl/edu/agh/soa/HelloWorld.java
@@ -1,15 +1,51 @@
 package pl.edu.agh.soa;
 
+import org.jboss.ejb3.annotation.SecurityDomain;
+import org.jboss.ws.api.annotation.WebContext;
+
+import javax.annotation.security.DeclareRoles;
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @WebService
 @Stateless
+@DeclareRoles({"developer"})
+@WebContext(authMethod = "BASIC",transportGuarantee = "NONE")
+@SecurityDomain("testdomeny")
 public class HelloWorld {
-    @WebMethod
+//    @WebMethod
+//    public String hello(@WebParam(name = "name") String name){
+//        return "Hello " + name;
+//    }
+
+//    @WebMethod
+//    @RolesAllowed("abc")
+//    @XmlElementWrapper(name="ElementsWraper")
+//    @XmlElement(name="Element")
+//    public List Hi(@WebParam(name = "name")String name){
+//        List<String> list = new ArrayList<>();
+//        list.add("Hello " + name);
+//        list.add("Cześć " + name);
+//        list.add("Guten Tag " + name);
+//
+//        return list;
+//    }
+
+    @WebMethod(action = "Hello")
+    @WebResult(name = "getHello")
+    @RolesAllowed("developer")
     public String hello(@WebParam(name = "name") String name){
         return "Hello " + name;
     }
+
+
 }
